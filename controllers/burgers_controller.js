09852b8f9eefe71d1505d130724009c51eff5301
klwidmer <klwidmer@gmail.com>
2018-05-17 12:14:27 -0500

    initial

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,14 @@
+const experss = require("express")
+
+const router = experss.Router();
+
+router.get("/foobar", function(req, res) {
+    res.send(
+
+  <script src="/reload/reload.js"></script> 
+);
+});
+
+
+
+module.exports = router
---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+ id AUTO_INCREMENT PRIMARY KEY,
+ burger_name VARCHAR (45),
+ devoured Boolean
+);
